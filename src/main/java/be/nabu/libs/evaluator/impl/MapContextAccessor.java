c94303272aead010a41a19302d9ec9ee012a5a2a
alex <alex@nabu.be>
2015-08-27 07:59:07 +0200

    Make map context accessor listable

--- src/main/java/be/nabu/libs/evaluator/impl/MapContextAccessor.java
+++ src/main/java/be/nabu/libs/evaluator/impl/MapContextAccessor.java
@@ -1,12 +1,15 @@
 package be.nabu.libs.evaluator.impl;
 
+
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
 
 import be.nabu.libs.evaluator.EvaluationException;
-import be.nabu.libs.evaluator.api.ContextAccessor;
+import be.nabu.libs.evaluator.api.ListableContextAccessor;
 
 @SuppressWarnings("rawtypes")
-public class MapContextAccessor implements ContextAccessor<Map> {
+public class MapContextAccessor implements ListableContextAccessor<Map> {
 
 	@Override
 	public Class<Map> getContextType() {
@@ -21,6 +24,12 @@ public class MapContextAccessor implements ContextAccessor<Map> {
 	@Override
 	public Object get(Map context, String name) throws EvaluationException {
 		return context.get(name);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public Collection<String> list(Map object) {
+		return object == null ? new ArrayList<String>() : new ArrayList<String>(object.keySet());
 	}
 
 }
